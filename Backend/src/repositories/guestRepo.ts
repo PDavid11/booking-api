415134b3daac2f428e33bcc887f5a5d7a113de2b
Pap Dávid <paapdaavee@hotmail.com>
2026-03-18 20:59:40 +0100

    feat: add guestRepo

---
+++ Backend/src/repositories/guestRepo.ts
@@ -0,0 +1,45 @@
+import type { Gender } from "../enums/enums.js";
+import { Guest } from "../models/Guest.js";
+
+export class GuestRepo {
+
+    private guest : Guest[]
+
+    constructor(guest : Guest[] = []) {
+        this.guest = guest
+    }
+
+    get Guest() {return this.guest}
+
+    getByID(ID: string): {success: boolean, reason?: string, result?: Guest} {
+        let result = this.Guest.find(r => r.ID === ID)
+        if (!result) {
+            return {success: false, reason: "Not found"}          
+        } else {
+            return {success: true, result: result}
+        }
+    }
+
+    getByPhone(phone: string): {success: boolean, reason?: string, result?: Guest} {
+        let result = this.Guest.find(r => r.Phone === phone)
+        if (!result) {
+            return {success: false, reason: "Not found"}    
+        } else {
+            return {success: true, result: result}
+        }
+    }
+
+    add(name: string, phone: string, gender: Gender) {
+        this.guest.push(new Guest(name, phone, gender))
+    }
+
+    delete(ID: string): {success: boolean, reason?: string} {
+        let index = this.guest.findIndex(i => i.ID === ID)
+        if (index === -1) {
+            return { success: false, reason: "Not found"}
+        } else {
+            this.guest.splice(index, 1)
+            return { success: true }
+        }
+    }
+}
