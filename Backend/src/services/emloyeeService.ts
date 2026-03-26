6448ee1a5db8a78cc711b975d0d7aeeaac4a0fcd
Pap Dávid <paapdaavee@hotmail.com>
2026-03-26 22:02:55 +0100

    feat: add employeeService

---
+++ Backend/src/services/emloyeeService.ts
@@ -0,0 +1,31 @@
+import type { EmployeeRole } from "../enums/enums.js";
+import { EmployeeRepo } from "../repositories/employeeRepo.js";
+
+export class EmployeeService {
+
+    private employeeService : EmployeeRepo
+
+    constructor(employeeService: EmployeeRepo) {
+        this.employeeService = employeeService
+    }
+
+    add(name: string, phone: string, instagram: string, role: EmployeeRole) {
+        this.employeeService.add(name, phone, instagram, role)
+    }
+
+    modifyName(ID: string, newName: string): {success: boolean, reason?: string} {
+        return this.employeeService.modifyName(ID, newName)
+    }
+
+    modifyPhone(ID: string, newPhone: string): {success: boolean, reason?: string} {
+        return this.employeeService.modifyPhone(ID, newPhone)
+    }
+
+    modifyInstagram(ID: string, newInstagram: string): {success: boolean, reason?: string} {
+        return this.employeeService.modifyInstagram(ID, newInstagram)
+    }
+
+    modifyRole(ID: string, newRole: EmployeeRole): {success: boolean, reason?: string} {
+        return this.employeeService.modifyRole(ID, newRole)
+    }
+}
