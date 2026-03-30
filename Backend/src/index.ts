e93db338bc87dcfb775c379871094caa4088db0f
Pap Dávid <paapdaavee@hotmail.com>
2026-03-30 21:35:40 +0200

    feat: connect routes to Express server

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import EmployeeRoutes from "./routes/employeeRoutes.js"
 
 const app = express()
 const PORT = 3000
+app.use(express.json())
+app.use('/employees', EmployeeRoutes)
 
 app.get("/health", (req, res) => {
     res.json({status: "ok"})
