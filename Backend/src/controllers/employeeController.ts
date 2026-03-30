6d65131630d5866b320841de33393ca3b884caea
Pap Dávid <paapdaavee@hotmail.com>
2026-03-30 21:35:15 +0200

    feat: add employeeController

---
+++ Backend/src/controllers/employeeController.ts
@@ -0,0 +1,40 @@
+import { EmployeeService } from "../services/employeeService.js";
+import type { Request, Response } from "express";
+
+export class EmployeeController {
+
+    private employeeController : EmployeeService
+
+    constructor(employeeController : EmployeeService) {
+        this.employeeController = employeeController
+    }
+
+    add(req: Request, res: Response) {
+        const {name, phone, instagram, role} = req.body
+        this.employeeController.add(name, phone, instagram, role)
+        res.status(201).json({success: true})
+    }
+
+    delete(req: Request, res: Response) {
+        const { ID } = req.body
+        const result = this.employeeController.delete(ID)
+        if (!result.success) {
+            res.status(404).json(result)
+        } else {
+            res.status(200).json(result)
+        }
+    }
+
+    modify(req: Request, res: Response) {
+        const { ID, name, phone, instagram, role } = req.body
+        const NN = this.employeeController.modifyName(ID, name)
+        const NP = this.employeeController.modifyPhone(ID, phone)
+        const NI = this.employeeController.modifyInstagram(ID, instagram)
+        const NR = this.employeeController.modifyRole(ID, role)
+        if (NN.success && NP.success && NI.success && NR.success) {
+            res.status(200).json({success: true})
+        } else {
+            res.status(400).json({success: false})
+        }
+    }
+}
